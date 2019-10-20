918d8504fc701a75ef7955f9e517808a5acfd120
Nikita <hekitko0@gmail.com>
2019-10-20 14:09:50 +0300

    Hospital.UI add Autofac

---
+++ Hospital.UI/App_Start/AutofacConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace Hospital.UI
+{
+    public class AutofacConfig
+    {
+        public static void RegisterAutofacModules()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new DAL.DataLayer());
+            builder.RegisterModule(new Services.ServiceLayer());
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
